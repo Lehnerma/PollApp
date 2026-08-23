3dbe24cdb11e6d541d48e15f6ab3cfa800226a6b
Max Lehner <dev@lm-lehner.at>
2026-08-23 22:59:18 +0200

    feat: implement questions list editor component with add question functionality fix: styling in the question editor feat: creat BEM btn__add class and --icon to the main styling

---
+++ poll-app/src/app/shared/components/questions-list-editor/questions-list-editor.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { QuestionEditor } from '../question-editor/question-editor';
+
+@Component({
+  selector: 'app-questions-list-editor',
+  imports: [QuestionEditor],
+  templateUrl: './questions-list-editor.html',
+  styleUrl: './questions-list-editor.scss',
+})
+export class QuestionsListEditor {
+  // addquestion
+  //delete question > 1
+  //reset question
+}

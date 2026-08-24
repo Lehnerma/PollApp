6a699c0c7a4ccdf8e0b6f320ed93a4d170db8f91
Max Lehner <dev@lm-lehner.at>
2026-08-24 23:02:10 +0200

    fix little styling problems - switch the btn for the next question into the ul elemten like in figam, and set a with to contentn-fit

--- poll-app/src/app/shared/components/survey-detail-form/survey-detail-form.ts
+++ poll-app/src/app/shared/components/survey-detail-form/survey-detail-form.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { DropdownComponent } from '../dropdown-component/dropdown-component';
 
 @Component({
-  selector: 'app-survey-detail-form',
+  selector: 'survey-detail-form',
   imports: [DropdownComponent],
   templateUrl: './survey-detail-form.html',
   styleUrl: './survey-detail-form.scss',

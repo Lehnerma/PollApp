64c853da996a80d39fdaaad4def19a79b0252a8b
Max Lehner <dev@lm-lehner.at>
2026-08-25 13:05:45 +0200

    fix: update form controls and structure in survey components

--- poll-app/src/app/shared/components/questions-list-editor/questions-list-editor.ts
+++ poll-app/src/app/shared/components/questions-list-editor/questions-list-editor.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { QuestionEditor } from '../question-editor/question-editor';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-questions-list-editor',
@@ -9,38 +9,5 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './questions-list-editor.scss',
 })
 export class QuestionsListEditor {
-  fb = inject(FormBuilder);
-  questions = this.fb.array([this.createQuestion()]);
-
-  /**
-   *
-   * @returns
-   */
-  createQuestion(): FormGroup {
-    return this.fb.group({
-      question: ['', Validators.required],
-      multipleOptions: ['false'],
-      options: this.fb.array([
-        this.createOption(), //
-        this.createOption(),
-      ]),
-    });
-  }
-
-  /**
-   * add
-   */
-  addNewQuestion(): void {
-    this.questions.push(this.createQuestion());
-  }
-
-  /**
-   * Creats a new option input for the question
-   * @returns a new formgroup input element for a option
-   */
-  createOption(): FormGroup {
-    return this.fb.group({
-      text: ['', Validators.required],
-    });
-  }
+  questions = input.required<FormArray>();
 }
--- poll-app/src/app/shared/components/survey-detail-form/survey-detail-form.ts
+++ poll-app/src/app/shared/components/survey-detail-form/survey-detail-form.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { DropdownComponent } from '../dropdown-component/dropdown-component';
+import { FormGroup, ɵInternalFormsSharedModule } from '@angular/forms';
 
 @Component({
   selector: 'survey-detail-form',
-  imports: [DropdownComponent],
+  imports: [DropdownComponent, ɵInternalFormsSharedModule],
   templateUrl: './survey-detail-form.html',
   styleUrl: './survey-detail-form.scss',
 })
 export class SurveyDetailForm {
-  list = ['test', 'categories', 'hello welt'];
+  categories = input([]);
+  detailForm = input.required<FormGroup>();
 }

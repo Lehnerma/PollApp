0879d319a682bfd4a9dd001531af2bae85cb0b8d
Max Lehner <dev@lm-lehner.at>
2026-08-25 09:11:34 +0200

    formating and styling fixes

--- poll-app/src/app/shared/pages/dev/dev.ts
+++ poll-app/src/app/shared/pages/dev/dev.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { SurveyCreateComponent } from "../../components/survey-create-component/survey-create-component";
-
+import { SurveyCreateComponent } from '../../components/survey-create-component/survey-create-component';
 
 @Component({
   selector: 'app-dev',

6540c65158504e7c8956897847bfc1323df4cfc5
Max Lehner <dev@lm-lehner.at>
2026-09-05 10:19:14 +0200

    feat: refactor checkbox component to use reactive form inputs and update bindings

--- poll-app/src/app/shared/components/checkbox-component/checkbox-component.ts
+++ poll-app/src/app/shared/components/checkbox-component/checkbox-component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-component',
@@ -6,16 +6,19 @@ import { Component, input, signal } from '@angular/core';
   templateUrl: './checkbox-component.html',
   styleUrl: './checkbox-component.scss',
 })
-export class CheckboxComponent{
+export class CheckboxComponent {
+  type = input<'checkbox' | 'radio'>('checkbox');
+  name = input<string>(''); // Gruppenname – bei Radios pro Frage gleich
+  value = input<string>(''); // option.id
   label = input<string>();
-  id = input();
-  checked = signal(false);
-
+  id = input<string>(''); // aktuell untypisiert -> unknown
+  checked = input(false); // State liegt beim Parent
+  changed = output<string>();
 
   /**
    * Toggles the checkbox value and emits the new state.
    */
-  toggle(): void {
-    this.checked.set(!this.checked());
-  }
+  // toggle(): void {
+  //   this.checked.set(!this.checked());
+  // }
 }

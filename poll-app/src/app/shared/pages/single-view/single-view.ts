9157b22be4486d5364090831f11188e9e78d9ae1
Max Lehner <dev@lm-lehner.at>
2026-09-06 21:57:44 +0200

    feat: enhance live results layout and styling, add main header to single view

--- poll-app/src/app/shared/pages/single-view/single-view.ts
+++ poll-app/src/app/shared/pages/single-view/single-view.ts
@@ -3,10 +3,11 @@ import { FillOut } from '../../components/fillout/fillout';
 import { LiveResults } from '../../components/live-results/live-results';
 import { ActivatedRoute } from '@angular/router';
 import { SupabaseService } from '../../services/supabase-service';
+import { MainHeader } from '../../components/main-header/main-header';
 
 @Component({
   selector: 'app-single-view',
-  imports: [FillOut, LiveResults],
+  imports: [FillOut, LiveResults, MainHeader],
   templateUrl: './single-view.html',
   styleUrl: './single-view.scss',
 })

2d66356de9dfad8f65ea489184516c4f740f43df
Max Lehner <dev@lm-lehner.at>
2026-09-09 02:34:26 +0200

    feat: add animated phone logo component and update main hero section

--- poll-app/src/app/shared/components/main-hero/main-hero.ts
+++ poll-app/src/app/shared/components/main-hero/main-hero.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { App } from '../../../app';
+import { PhoneLogo } from '../phone-logo/phone-logo';
 
 @Component({
   selector: 'app-main-hero',
-  imports: [],
+  imports: [PhoneLogo],
   templateUrl: './main-hero.html',
   styleUrl: './main-hero.scss',
 })
---
+++ poll-app/src/app/shared/components/phone-logo/phone-logo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'phone-logo',
+  imports: [],
+  templateUrl: './phone-logo.html',
+  styleUrl: './phone-logo.scss',
+})
+export class PhoneLogo {}
